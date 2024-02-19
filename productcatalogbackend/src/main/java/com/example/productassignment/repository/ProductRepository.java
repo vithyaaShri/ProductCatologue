0a0ec1e941baa335847c5224f1dc2e7cec4a721a
vithyaaShri <153651430+vithyaaShri@users.noreply.github.com>
2024-02-19 12:20:26 +0400

    Update ProductRepository.java

--- productcatalogbackend/src/main/java/com/example/productassignment/repository/ProductRepository.java
+++ productcatalogbackend/src/main/java/com/example/productassignment/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package com.example.productassignment.repository;
 import com.example.productassignment.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//Repository class and gets the methods from JpaRepository Class
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
